809c381b7ad8915b524e7ca4a93e77e08b08ee64
Diego Parra <28814932+diegoapb@users.noreply.github.com>
2025-08-03 21:04:26 -0500

    Remove commented code and delete unused test file

--- lib/fast-api-docker-ecs-stack.ts
+++ lib/fast-api-docker-ecs-stack.ts
@@ -6,11 +6,5 @@ export class FastApiDockerEcsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'FastApiDockerEcsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
