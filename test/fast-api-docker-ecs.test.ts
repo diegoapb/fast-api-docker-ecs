04223bb41f8d62f70c3dfe3e101966eabb56c780
Diego <diego@Diegos-Mac-mini.local>
2025-08-03 20:41:44 -0500

    Initial commit

---
+++ test/fast-api-docker-ecs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FastApiDockerEcs from '../lib/fast-api-docker-ecs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fast-api-docker-ecs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FastApiDockerEcs.FastApiDockerEcsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
